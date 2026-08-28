3e8e04cb8540059958ff46c63fca078e51f66aa7
Bibek Giri <giribibek01@gmail.com>
2026-08-28 19:02:26 +0545

    Created a Todo List Flutter project
    - Created CMakeLists.txt for Windows build configuration.
    - Added Flutter window management with Win32 API integration.
    - Implemented resource files including icons and application manifest.
    - Established plugin registration mechanism for Flutter plugins.
    - Introduced console output redirection for debugging.
    - Added .gitignore for Windows-specific files.
    - Created a web manifest for the Flutter web application.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
